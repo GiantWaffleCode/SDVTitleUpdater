d31caa9daf4103cb4085bdf0f8859e814dd3afc8
GiantWaffleCode <72772325+GiantWaffleCode@users.noreply.github.com>
2024-01-30 22:16:32 -0600

    init and main script

--- script.py
+++ script.py
@@ -1,1 +1,79 @@
-#Title Script
+#Imports
+import idsecrets
+import xml.etree.ElementTree as ET
+from twitchAPI.twitch import Twitch
+from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.type import AuthScope
+import asyncio
+import time
+
+
+def gen_title():
+    #Import XML File
+    tree = ET.parse(r"C:\Users\waffl\AppData\Roaming\StardewValley\Saves\Clickbait_366212092\SaveGameInfo")
+    root = tree.getroot()
+
+    #Setup XML Attribute Names
+    day = 'dayOfMonthForSaveGame'
+    season = 'seasonForSaveGame'
+    year = 'yearForSaveGame'
+
+    #Vars
+    current_day_val = None
+    current_season_val = None
+    current_year_val = None
+
+    #Season Dictionary
+    seasons = {
+        '0' : 'Spring',
+        '1' : 'Summer',
+        '2' : 'Fall',
+        '3' : 'Winter'
+    }
+
+    #Search Element Tree for Data
+    for child in root:
+        if child.tag == day:
+            current_day_val = child.text
+        elif child.tag == season:
+            current_season_val = child.text
+        elif child.tag == year:
+            current_year_val = child.text
+
+    TITLE_STRING = f'{seasons[current_season_val]} {current_day_val} - Year {current_year_val} | Stardew Valley Expanded Mod | !sdv'
+    #print(TITLE_STRING)
+
+    return TITLE_STRING
+
+#Import Secrets
+app_id = idsecrets.id
+app_secret = idsecrets.secret
+channel_id = idsecrets.channel
+
+#Function to Set Title
+async def set_title(TITLE_STRING):
+    twitch = await Twitch(app_id, app_secret)
+
+    target_scope = [AuthScope.CHANNEL_MANAGE_BROADCAST]
+    auth = UserAuthenticator(twitch, target_scope, force_verify=False)
+    # this will open your default browser and prompt you with the twitch verification website
+    token, refresh_token = await auth.authenticate()
+    # add User authentication
+    await twitch.set_user_authentication(token, target_scope, refresh_token)
+    #Set Title
+    await twitch.modify_channel_information(channel_id, title=TITLE_STRING)
+
+#Main Loop
+while True:
+    now = time.time()
+
+    unixid = 'id-' + str(now)[-5:]
+
+    if int(now) % 60 == 0:
+        print(f'Getting Title String')
+        title = gen_title()
+        titleid = f'{title} | {unixid}'
+        print(f'Title = {titleid}')
+        asyncio.run(set_title(titleid)) 
+        print(f'Title Set Complete')
+    time.sleep(1)
