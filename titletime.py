962a2f6c6219c02e56d8a1cfe51c9ff38e328024
GiantWaffleCode <72772325+GiantWaffleCode@users.noreply.github.com>
2024-02-07 08:16:01 -0600

    fixed 0 minute bug

--- titletime.py
+++ titletime.py
@@ -6,12 +6,13 @@ from twitchAPI.type import AuthScope
 import asyncio
 import time
 from datetime import datetime
+import math
 
 #?Maybe use Gradio for this aswell
 
 def get_time_delta():
     current_time = datetime.now()
-    stream_start_time = datetime.strptime("07/02/24 07:00", "%d/%m/%y %H:%M")
+    stream_start_time = datetime.strptime("07/02/24 09:00", "%d/%m/%y %H:%M")
 
     #print('Current Time:', current_time.time())
     #print('Stream Start Time:', stream_start_time.time())
@@ -19,20 +20,23 @@ def get_time_delta():
     delta_time = stream_start_time - current_time
     time_left_seconds = delta_time.total_seconds()
 
-    time_left_minutes = round((time_left_seconds / 60) % 60)
+    time_left_minutes = math.floor((time_left_seconds / 60) % 60)
 
-    time_left_hours = round(time_left_seconds / 3600)
+    time_left_hours = math.floor(time_left_seconds / 3600)
+
     #print(f'Hrs:{time_left_hours} Mins:{time_left_minutes}')
 
     return time_left_hours, time_left_minutes
 
 def gen_title():
     hrs, mins = get_time_delta()
-    if mins != 60:
-        TITLE_STRING = f"Live in {hrs} hours and {mins} minutes. Unless I'm late..."
-    else:
+    if hrs == 0:
+        TITLE_STRING = f"Live in {mins} minutes. Unless I'm late..."
+    elif mins == 0:
         TITLE_STRING = f"Live in {hrs} hours. Unless I'm late..."
-
+    else:
+        TITLE_STRING = f"Live in {hrs} hours and {mins} minutes. Unless I'm late..."
+        
     #print(TITLE_STRING)
 
     return TITLE_STRING
@@ -59,14 +63,14 @@ async def set_title(twitch, TITLE_STRING):
     #Set Title
     await twitch.modify_channel_information(channel_id, title=TITLE_STRING)
 
-
-
 #Main Loop
 
 #Auth with Twitch
 print('Authing...')
-twitch = asyncio.run(auth())
+#twitch = asyncio.run(auth())
 print(f'Auth Complete...')
+print(gen_title())
+
 
 while True:
     now = time.time()
@@ -75,6 +79,6 @@ while True:
         print(f'Getting Title String')
         title = gen_title()
         print(f'Title = {title}')
-        asyncio.run(set_title(twitch, title)) 
+        #asyncio.run(set_title(twitch, title)) 
         print(f'Title Set Complete')
     time.sleep(1)
